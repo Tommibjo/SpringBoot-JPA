6f0e02e0d7c20e93b05459fb13865e85f394043d
Tommi <tommi.bjrklund@gmail.com>
2018-07-18 22:42:51 +0300

    Base code for Spring Boot applications

---
+++ src/main/java/com/yritys/springbootbase/controllers/AppController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.yritys.springbootbase.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Tommi
+ */ 
+@Controller
+public class AppController {
+    
+    @RequestMapping("/")
+    public String home(){
+        return "index";
+    }
+}
