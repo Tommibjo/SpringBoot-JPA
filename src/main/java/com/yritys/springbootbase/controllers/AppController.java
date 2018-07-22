160957b211a3adb8cef5a601c76d40c296ae9c82
Tommi <tommi.bjrklund@gmail.com>
2018-07-22 23:31:35 +0300

    <b>Front:</b> hmtl, thymeleaf, css <b>Backend:</b> Springboot & H2Database

--- src/main/java/com/yritys/springbootbase/controllers/AppController.java
+++ src/main/java/com/yritys/springbootbase/controllers/AppController.java
@@ -5,18 +5,47 @@
  */
 package com.yritys.springbootbase.controllers;
 
+import com.yritys.springbootbase.jpa.User;
+import com.yritys.springbootbase.jpa.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
  * @author Tommi
- */ 
+ */
 @Controller
 public class AppController {
-    
+
+    @Autowired
+    private UserRepository userrepository;
+
     @RequestMapping("/")
-    public String home(){
+    public String home(Model model) {
         return "index";
     }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String getForm(@RequestParam String name, @RequestParam String surname, @RequestParam int age, @RequestParam String gender) {
+        userrepository.saveAndFlush(new User(name, surname, gender, age));
+        return "redirect:/";
+    }
+
+    @RequestMapping("/users")
+    public String users(Model model) {
+        model.addAttribute("userList", userrepository.findAll());
+        
+        return "users";
+    }
+    
+    @RequestMapping("/user")
+    public String user(@RequestParam String user, Model model){
+        model.addAttribute("user", user);
+        return "users";
+    }
+
 }
